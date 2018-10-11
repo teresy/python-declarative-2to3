967209e4ffb857e92c3d1b277d97f1b6d2f144c0
Rijnard van Tonder <rvantonder@gmail.com>
2018-10-11 19:10:58 -0400

    add next comparison

---
+++ test-equal-next.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd before-refactor-2to3-workspace
+git checkout -- .
+time 2to3 -f next -j 20 -w --no-diffs -n . &> /dev/null
+git checkout -- .
+cd ..
+
+cd before-refactor-teresy-workspace
+git checkout -- .
+~/rooibos-future/main -verbose -match-timeout 1 -d $(pwd) -f .py -templates `echo ../templates/next/* | tr ' ' ,`
+git checkout -- .
+cd ..
+
+EQUAL=`./diff-teresy-vs-2to3.sh`
+
+echo "Diffs" ${EQUAL}
