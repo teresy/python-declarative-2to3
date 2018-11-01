7f2be746eeabe83b5de4fc012f416ab509e6ba3d
Rijnard van Tonder <rvantonder@gmail.com>
2018-11-01 21:13:03 +0000

    update scripts

--- 0-test-equal-next.sh
+++ 0-test-equal-next.sh
@@ -3,15 +3,21 @@
 cd before-refactor-2to3-workspace
 git checkout -- .
 time 2to3 -f next -j 20 -w --no-diffs -n . &> /dev/null
-git checkout -- .
 cd ..
 
 cd before-refactor-teresy-workspace
 git checkout -- .
 time ~/rooibos-future/main -verbose -match-timeout 1 -d $(pwd) -f .py -templates `echo ../templates/next/* | tr ' ' ,`
-git checkout -- .
 cd ..
 
 EQUAL=`./diff-teresy-vs-2to3.sh`
 
 echo "Diffs" ${EQUAL}
+
+cd before-refactor-2to3-workspace
+git checkout -- .
+cd ..
+
+cd before-refactor-teresy-workspace
+git checkout -- .
+cd ..
